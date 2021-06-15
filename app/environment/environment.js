32c842a8017833258b34a077465da648b3c3272f
Leonardo <leonardocornejoruiz@gmail.com>
2021-06-15 12:57:25 -0500

    environment frontend added

--- app/environment/environment.js
+++ app/environment/environment.js
@@ -1,2 +1,2 @@
-export const restapi_url = "https://rest-nutricam.azurewebsites.net/api";
-export const recognizeapi_url = "https://recognize-api.azurewebsites.net/";
+export const restapi_url = "https://nutri-rest-api.azurewebsites.net" + "/api";
+export const recognizeapi_url = "https://nutri-recognize-api.azurewebsites.net";
--- backend/app/db.py
+++ backend/app/db.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 conn = db.connect(
-    os.getenv('CONNECTION_DB') or 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:nutriserver.database.windows.net;UID=nutricam_tp;Database=tp_project;PWD=memo#pry2020217;' 
+    os.getenv('CONNECTION_DB') or 'Driver={ODBC Driver 17 for SQL Server};Server=LAPTOP-ABRG7UHS\MSSQLSERVER01;UID=nutricam_tp;Database=tp_project;PWD=memo#pry2020217;' 
 )
 
 cursor = conn.cursor()
